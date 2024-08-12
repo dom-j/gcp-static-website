0047a15d83f6e35695d1d9531d987275189b15a1
dominikjaro <dominikjaro789@gmail.com>
2024-08-12 08:46:47 +0100

    Comment the rules

--- infra/main.tf
+++ infra/main.tf
@@ -160,20 +160,20 @@ resource "google_compute_url_map" "website" {
 }
 #GCP HTTPS Proxy
 
-resource "google_compute_target_https_proxy" "website" {
-  provider = google  
-  name = "website--target-proxy-uk"
-  url_map = google_compute_url_map.website.self_link
-  ssl_certificates = [google_compute_managed_ssl_certificate.website.self_link]
-}
+# resource "google_compute_target_https_proxy" "website" {
+#   provider = google  
+#   name = "website--target-proxy-uk"
+#   url_map = google_compute_url_map.website.self_link
+#   ssl_certificates = [google_compute_managed_ssl_certificate.website.self_link]
+# }
 
-#GCP forwarding rule 
-resource "google_compute_global_forwarding_rule" "default" {
-    provider = google
-    name = "website-forarding-rule"
-    load_balancing_scheme = "EXTERNAL"
-    ip_address = google_compute_global_address.website_ip.address
-    ip_protocol = "TCP"
-    port_range = "443"
-    target = google_compute_target_https_proxy.website.self_link
-}
+# #GCP forwarding rule 
+# resource "google_compute_global_forwarding_rule" "default" {
+#     provider = google
+#     name = "website-forarding-rule"
+#     load_balancing_scheme = "EXTERNAL"
+#     ip_address = google_compute_global_address.website_ip.address
+#     ip_protocol = "TCP"
+#     port_range = "443"
+#     target = google_compute_target_https_proxy.website.self_link
+# }
